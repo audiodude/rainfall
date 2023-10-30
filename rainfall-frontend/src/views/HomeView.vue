4efd6ac41cf723296ffe51b34afbbcccc606b0e1
Travis Briggs <audiodude@gmail.com>
2023-10-29 22:21:44 -0700

    Remove template cruft from frontend

--- rainfall-frontend/src/views/HomeView.vue
+++ rainfall-frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld />
   </main>
 </template>
