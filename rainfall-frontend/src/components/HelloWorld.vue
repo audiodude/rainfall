8c2034b401cb2a41d9aa31fe110c599cd547b990
Travis Briggs <audiodude@gmail.com>
2023-10-30 11:04:22 -0700

    Add google login

--- rainfall-frontend/src/components/HelloWorld.vue
+++ rainfall-frontend/src/components/HelloWorld.vue
@@ -3,6 +3,8 @@ export default {
   data() {
     return {
       name: 'Beta',
+      clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+      redirectUri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
     };
   },
 };
@@ -10,6 +12,13 @@ export default {
 
 <template>
   <h1>Rainfall {{ name }}</h1>
+  <div
+    id="g_id_onload"
+    :data-client_id="clientId"
+    data-ux_mode="redirect"
+    :data-login_uri="redirectUri"
+  ></div>
+  <div class="g_id_signin" data-type="standard"></div>
 </template>
 
 <style scoped></style>
