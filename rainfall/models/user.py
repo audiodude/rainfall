e98e215bb5d8cac77913c046738cb7af82416384
Travis Briggs <audiodude@gmail.com>
2023-10-30 12:01:15 -0700

    Add SQLAlchemy, SQlAlchemy-Flask, python-dotenv, and define the first iteration of the User model.

---
+++ rainfall/models/user.py
@@ -0,0 +1,17 @@
+from functools import partial
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.types import Uuid, String
+from uuid_extensions import uuid7
+
+from rainfall.db import Base
+
+
+class User(Base):
+  __tablename__ = 'users'
+
+  id: Mapped[bytes] = mapped_column(Uuid, primary_key=True, default=uuid7)
+  google_id: Mapped[str] = mapped_column(String(255))
+
+  def __repr__(self) -> str:
+    return f'User(id={self.id!r}, google_id={self.google_id!r})'
