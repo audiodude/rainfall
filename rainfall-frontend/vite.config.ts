23ac6dfd732542a406d64a1e6bdc4089847e1d27
Travis Briggs <audiodude@gmail.com>
2023-10-29 22:03:49 -0700

    Proxy requests to API

--- rainfall-frontend/vite.config.ts
+++ rainfall-frontend/vite.config.ts
@@ -5,12 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api/v1': 'http://localhost:5000'
     }
   }
 })
