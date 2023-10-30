2e9a5d5a50215944382510249e58786e00e45252
Travis Briggs <audiodude@gmail.com>
2023-10-30 11:04:22 -0700

    Add google login

--- rainfall/main.py
+++ rainfall/main.py
@@ -2,6 +2,22 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def index():
   return 'Hello flask'
+
+
+@app.route('/api/v1/login', methods=['POST'])
+def login():
+  csrf_token_cookie = flask.request.cookies.get('g_csrf_token')
+  if not csrf_token_cookie:
+    return flask.jsonify(status=400, error='No CSRF token in Cookie.'), 400
+  csrf_token_body = flask.request.form.get('g_csrf_token')
+  if not csrf_token_body:
+    return flask.jsonify(status=400, error='No CSRF token in post body.'), 400
+  if csrf_token_cookie != csrf_token_body:
+    return flask.jsonify(status=400,
+                         error='Failed to verify double submit cookie.'), 400
+
+  return flask.redirect('/')
