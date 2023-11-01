9ae7d5d86f271b14f67c0f724ca4a3d2223f7625
Travis Briggs <audiodude@gmail.com>
2023-10-31 17:38:32 -0700

    Refactor and simplify handler code. Create helper module that is testable

--- rainfall/main.py
+++ rainfall/main.py
@@ -5,12 +5,9 @@ from urllib.parse import urljoin
 from google.oauth2 import id_token
 from google.auth.transport import requests as goog_requests
 import flask
-import jwt
-import sqlalchemy
-from sqlalchemy import select
 
 from rainfall.db import db
-from rainfall.models.user import User
+from rainfall.login import check_csrf, save_or_update_google_user
 
 app = flask.Flask(__name__)
 
@@ -39,48 +36,18 @@ def get_user():
 
 @app.route('/api/v1/login', methods=['POST'])
 def login():
-  csrf_token_cookie = flask.request.cookies.get('g_csrf_token')
-  if not csrf_token_cookie:
-    return flask.jsonify(status=400, error='No CSRF token in Cookie.'), 400
-  csrf_token_body = flask.request.form.get('g_csrf_token')
-  if not csrf_token_body:
-    return flask.jsonify(status=400, error='No CSRF token in post body.'), 400
-  if csrf_token_cookie != csrf_token_body:
-    return flask.jsonify(status=400,
-                         error='Failed to verify double submit cookie.'), 400
+  resp = check_csrf()
+  if resp:
+    return resp
 
   token = flask.request.form.get('credential')
-  try:
-    jwks_client = jwt.PyJWKClient('https://www.googleapis.com/oauth2/v3/certs')
-    signing_key = jwks_client.get_signing_key_from_jwt(token)
-
-    data = jwt.decode(token,
-                      signing_key.key,
-                      algorithms="RS256",
-                      audience=GOOGLE_CLIENT_ID)
-  except jwt.exceptions.PyJWTError:
-    return flask.jsonify(status=400, error='Could not decrypt credential')
-
   try:
     idinfo = id_token.verify_oauth2_token(token, goog_requests.Request(),
                                           GOOGLE_CLIENT_ID)
   except ValueError:
     return flask.jsonify(status=400, error='Could not verify token')
 
-  stmt = select(User).filter_by(google_id=idinfo['sub'])
-  user = db.session.execute(stmt).scalar_one()
-
-  if user is None:
-    user = User(google_id=idinfo['sub'])
-
-  user.email = idinfo['email'],
-  user.name = idinfo['name'],
-  user.picture_url = idinfo['picture']
-  db.session.add(user)
-  db.session.flush()
-  user_id = user.id
-  db.session.commit()
-
+  user_id = save_or_update_google_user(idinfo)
   flask.session['user_id'] = user_id
 
   return flask.redirect(urljoin(RAINFALL_FRONTEND_URL, '/welcome'))
