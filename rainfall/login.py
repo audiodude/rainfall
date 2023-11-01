f03668dc9d0a3ad351264134f0712745fa29092b
Travis Briggs <audiodude@gmail.com>
2023-10-31 20:29:32 -0700

    First tests! Refactor app to create_app() pattern. Add pytest.ini and global fixtures.

--- rainfall/login.py
+++ rainfall/login.py
@@ -8,13 +8,13 @@ from rainfall.models.user import User
 def check_csrf():
   csrf_token_cookie = flask.request.cookies.get('g_csrf_token')
   if not csrf_token_cookie:
-    return flask.jsonify(status=400, error='No CSRF token in Cookie.'), 400
+    return flask.jsonify(status=400, error='No CSRF token in Cookie'), 400
   csrf_token_body = flask.request.form.get('g_csrf_token')
   if not csrf_token_body:
-    return flask.jsonify(status=400, error='No CSRF token in post body.'), 400
+    return flask.jsonify(status=400, error='No CSRF token in post body'), 400
   if csrf_token_cookie != csrf_token_body:
     return flask.jsonify(status=400,
-                         error='Failed to verify double submit cookie.'), 400
+                         error='Failed to verify double submit cookie'), 400
 
 
 def save_or_update_google_user(idinfo):
--- rainfall/main.py
+++ rainfall/main.py
@@ -10,45 +10,45 @@ from rainfall.db import db
 from rainfall.login import check_csrf, save_or_update_google_user
 from rainfall.models.user import User
 
-app = flask.Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
-app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
-db.init_app(app)
+def create_app():
+  app = flask.Flask(__name__)
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+  app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
+  db.init_app(app)
 
-GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
-RAINFALL_FRONTEND_URL = os.environ['RAINFALL_FRONTEND_URL']
+  GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
+  RAINFALL_FRONTEND_URL = os.environ['RAINFALL_FRONTEND_URL']
 
+  @app.route('/')
+  def index():
+    return 'Hello flask'
 
-@app.route('/')
-def index():
-  return 'Hello flask'
+  @app.route('/api/v1/user')
+  def get_user():
+    user_id = flask.session.get('user_id')
+    if user_id is None:
+      return flask.jsonify(status=404, error='No signed in user'), 404
 
+    user = db.session.get(User, user_id)
+    return flask.jsonify(user)
 
-@app.route('/api/v1/user')
-def get_user():
-  user_id = flask.session.get('user_id')
-  if user_id is None:
-    return flask.jsonify(status=404, error='No signed in user'), 404
+  @app.route('/api/v1/login', methods=['POST'])
+  def login():
+    resp = check_csrf()
+    if resp:
+      return resp
 
-  user = db.session.get(User, user_id)
-  return flask.jsonify(user)
+    token = flask.request.form.get('credential')
+    try:
+      idinfo = id_token.verify_oauth2_token(token, goog_requests.Request(),
+                                            GOOGLE_CLIENT_ID)
+    except ValueError:
+      return flask.jsonify(status=400, error='Could not verify token')
 
+    user_id = save_or_update_google_user(idinfo)
+    flask.session['user_id'] = user_id
 
-@app.route('/api/v1/login', methods=['POST'])
-def login():
-  resp = check_csrf()
-  if resp:
-    return resp
+    return flask.redirect(urljoin(RAINFALL_FRONTEND_URL, '/welcome'))
 
-  token = flask.request.form.get('credential')
-  try:
-    idinfo = id_token.verify_oauth2_token(token, goog_requests.Request(),
-                                          GOOGLE_CLIENT_ID)
-  except ValueError:
-    return flask.jsonify(status=400, error='Could not verify token')
-
-  user_id = save_or_update_google_user(idinfo)
-  flask.session['user_id'] = user_id
-
-  return flask.redirect(urljoin(RAINFALL_FRONTEND_URL, '/welcome'))
+  return app
