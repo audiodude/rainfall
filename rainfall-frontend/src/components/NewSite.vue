ae5d792ebc0ef5b4300d2ea50d7bf152c942edb8
Travis Briggs <audiodude@gmail.com>
2023-11-05 11:42:04 -0800

    Very basic "create_site" method, with tests

---
+++ rainfall-frontend/src/components/NewSite.vue
@@ -0,0 +1,66 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      createError: false,
+      name: '',
+    };
+  },
+  methods: {
+    async createSite() {
+      this.createError = false;
+      const resp = await fetch('/api/v1/site', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          site: {
+            name: this.name || 'My Cool Site',
+          },
+        }),
+      });
+      if (!resp.ok) {
+        setTimeout(() => {
+          this.createError = true;
+        }, 250);
+      }
+      this.$emit('site-created');
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div class="md:max-w-screen-md">
+      <div>
+        <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Site name</label
+        >
+        <input
+          v-model="name"
+          type="text"
+          class="max-w-xl bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="My Cool Site"
+          maxlength="255"
+          required
+        />
+      </div>
+      <p class="text-xs italic">
+        The title of your site is for your reference only and doesn't affect the output
+      </p>
+      <button
+        @click="createSite"
+        class="cursor-pointer mt-4 disabled:cursor-auto bg-transparent hover:bg-blue-500 disabled:hover:bg-transparent font-semibold hover:text-white disabled:hover:dark:text-gray-300 py-2 px-4 border border-blue-500 hover:border-transparent disabled:hover:border-blue-500 rounded"
+      >
+        Create site
+      </button>
+      <p v-if="createError" class="text-sm text-red-500">
+        Something went wrong while creating your site.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style></style>
--- rainfall/models/site.py
+++ rainfall/models/site.py
@@ -3,7 +3,7 @@ from functools import partial
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.types import Uuid
+from sqlalchemy.types import Uuid, String
 from uuid_extensions import uuid7
 
 from rainfall.db import db
@@ -16,6 +16,7 @@ class Site(db.Model):
   id: Mapped[bytes] = mapped_column(Uuid, primary_key=True, default=uuid7)
   user_id: Mapped[bytes] = mapped_column(ForeignKey("users.id"))
   user: Mapped["User"] = relationship(back_populates="sites")
+  name: Mapped[str] = mapped_column(String(255))
 
   def __repr__(self) -> str:
     return f'Site(id={self.id!r}, user_id={self.google_id!r})'
