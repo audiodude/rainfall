41ee33d0fa4c2f40970432f3deb1a3d483da9c68
Travis Briggs <audiodude@gmail.com>
2023-11-27 17:17:28 -0800

    More tests for releases

--- rainfall/blueprint/release_test.py
+++ rainfall/blueprint/release_test.py
@@ -1,7 +1,10 @@
+from uuid_extensions import uuid7
+
 import pytest
 
 from rainfall.conftest import BASIC_USER_ID
 from rainfall.db import db
+from rainfall.models.site import Site
 from rainfall.models.user import User
 
 
@@ -28,3 +31,96 @@ class ReleaseTest:
       releases = user.sites[0].releases
       assert len(releases) == 1
       assert releases[0].name == 'Release 1'
+
+  def test_create_no_user(self, app):
+    with app.test_client() as client:
+      rv = client.post(
+          '/api/v1/release',
+          json={'release': {
+              'name': 'Release 1',
+              'site_id': 'abc123'
+          }})
+      assert rv.status == '404 NOT FOUND'
+
+  def test_create_no_user_in_session(self, app, sites_user):
+    with app.test_client() as client:
+      rv = client.post(
+          '/api/v1/release',
+          json={'release': {
+              'name': 'Release 1',
+              'site_id': 'abc123'
+          }})
+      assert rv.status == '404 NOT FOUND'
+
+  def test_create_no_json(self, app, sites_user):
+    with app.test_client() as client:
+      with client.session_transaction() as sess:
+        sess['user_id'] = BASIC_USER_ID
+
+      rv = client.post('/api/v1/release')
+      assert rv.status == '415 UNSUPPORTED MEDIA TYPE'
+
+  def test_create_missing_release(self, app, sites_user):
+    with app.test_client() as client:
+      with client.session_transaction() as sess:
+        sess['user_id'] = BASIC_USER_ID
+
+      rv = client.post('/api/v1/release', json={'foo': 'bar'})
+      assert rv.status == '400 BAD REQUEST'
+
+  def test_create_missing_name(self, app, sites_user):
+    with app.test_client() as client:
+      with client.session_transaction() as sess:
+        sess['user_id'] = BASIC_USER_ID
+
+      rv = client.post('/api/v1/release', json={'release': {}})
+      assert rv.status == '400 BAD REQUEST'
+
+  def test_create_missing_site_id(self, app, sites_user):
+    with app.test_client() as client:
+      with client.session_transaction() as sess:
+        sess['user_id'] = BASIC_USER_ID
+
+      rv = client.post('/api/v1/release',
+                       json={'release': {
+                           'name': 'Release 1'
+                       }})
+      assert rv.status == '400 BAD REQUEST'
+
+  def test_create_site_not_exist(self, app, sites_user):
+    with app.test_client() as client:
+      with client.session_transaction() as sess:
+        sess['user_id'] = BASIC_USER_ID
+
+      rv = client.post('/api/v1/release',
+                       json={
+                           'release': {
+                               'name': 'Release 1',
+                               'site_id': uuid7(as_type='str')
+                           }
+                       })
+      assert rv.status == '404 NOT FOUND'
+
+  def test_create_site_user_mismatch(self, app, sites_user):
+    with app.app_context():
+      user = User(google_id='5678')
+      site = Site(name='Foo')
+      user.sites.append(site)
+      db.session.add(user)
+      db.session.add(site)
+      db.session.flush()
+      other_site_id = site.id
+      db.session.commit()
+
+    with app.test_client() as client:
+      with client.session_transaction() as sess:
+        sess['user_id'] = BASIC_USER_ID
+
+      rv = client.post('/api/v1/release',
+                       json={
+                           'release': {
+                               'name': 'Release 1',
+                               'site_id': str(other_site_id)
+                           }
+                       })
+      assert rv.status == '401 UNAUTHORIZED'
