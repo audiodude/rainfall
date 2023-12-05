25f92367406f766936a23c2d1d93a26204c9d993
Travis Briggs <audiodude@gmail.com>
2023-12-04 23:38:05 -0800

    Add link for filing questions/bugs

--- rainfall-frontend/src/App.vue
+++ rainfall-frontend/src/App.vue
@@ -18,6 +18,12 @@ import NavBar from './components/NavBar.vue';
         class="text-sm text-gray-900 dark:text-white"
         >Rain icons created by tulpahn - Flaticon</a
       >
+      <span
+        >Questions? Bugs?
+        <a class="text-blue-500 hover:underline" href="https://github.com/audiodude/rainfall/issues"
+          >File a Github issue</a
+        ></span
+      >
     </div>
   </footer>
 </template>
