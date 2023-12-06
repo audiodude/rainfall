a21cf88cafb1d4188a0c035eddf3f0805f3704a6
Travis Briggs <audiodude@gmail.com>
2023-12-05 16:43:26 -0800

    Enable ZIP file creation and download

--- rainfall/site.py
+++ rainfall/site.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 from uuid import UUID
 
@@ -37,8 +38,8 @@ def release_path(data_dir_path, release):
 
 
 def site_exists(preview_dir_path, site_id):
-  dir = build_dir(preview_dir_path, site_id)
-  return os.path.exists(dir) and len(os.listdir(dir)) > 0
+  dir_ = build_dir(preview_dir_path, site_id)
+  return os.path.exists(dir_) and len(os.listdir(dir_)) > 0
 
 
 def generate_site(data_dir_path, preview_dir_path, site_id):
@@ -54,3 +55,15 @@ def generate_site(data_dir_path, preview_dir_path, site_id):
   except subprocess.CalledProcessError as e:
     return (False, e.stderr.decode('utf-8'))
   return (True, None)
+
+
+def zip_file_path(preview_dir_path, site_id):
+  site = db.session.get(Site, UUID(site_id))
+  return os.path.join(preview_dir_path, str(site.user.id),
+                      secure_filename(site.name))
+
+
+def generate_zip(preview_dir_path, site_id):
+  root_dir = zip_file_path(preview_dir_path, site_id)
+  out_path = os.path.join(root_dir, 'rainfall_site')
+  shutil.make_archive(out_path, 'zip', root_dir=root_dir, base_dir='public')
