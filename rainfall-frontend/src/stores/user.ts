1308da4ac84d2e5ee993473ec770fcb4a9624609
Travis Briggs <audiodude@gmail.com>
2023-12-22 18:54:36 -0500

    Cache user promise so that API is not called multiple times on page load. Remove unnecessary call

--- rainfall-frontend/src/stores/user.ts
+++ rainfall-frontend/src/stores/user.ts
@@ -2,9 +2,10 @@ import { defineStore } from 'pinia';
 import { type User } from '../types/user';
 
 export const useUserStore = defineStore('user', {
-  state(): { user: User | null } {
+  state(): { user: User | null; userPromise: Promise<User | null> | null } {
     return {
       user: null,
+      userPromise: null,
     };
   },
   getters: {
@@ -15,12 +16,23 @@ export const useUserStore = defineStore('user', {
   actions: {
     async loadUser(force = false) {
       if (this.user && !force) {
-        return;
+        return this.user;
       }
-      const resp = await fetch('/api/v1/user');
-      if (resp.ok) {
-        this.user = await resp.json();
+
+      if (!this.userPromise) {
+        this.userPromise = fetch('/api/v1/user')
+          .then((resp) => {
+            if (resp.ok) {
+              return resp.json();
+            }
+            throw new Error(`Could not load user, response status=${resp.status}`);
+          })
+          .then((userData) => {
+            this.user = userData;
+            return this.user;
+          });
       }
+      return this.userPromise;
     },
     async signOut() {
       const resp = await fetch('/api/v1/logout');
