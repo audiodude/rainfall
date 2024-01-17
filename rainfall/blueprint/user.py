f368a63d12b1d2461e8ff3f36ec3b1d978c01a8a
Travis Briggs <audiodude@gmail.com>
2024-01-16 22:47:23 -0800

    First part of logging in with Mastodon: register an app with the server when the user types in the host

--- rainfall/blueprint/user.py
+++ rainfall/blueprint/user.py
@@ -1,15 +1,17 @@
 from dataclasses import fields
 import logging
 import os
-from urllib.parse import urljoin
+from urllib.parse import urlencode, urljoin, urlparse, urlunparse
 
 import flask
 from google.auth.transport import requests as goog_requests
 from google.oauth2 import id_token
+from sqlalchemy import select
 
 from rainfall.db import db
 from rainfall.decorators import with_current_site, with_current_user
-from rainfall.login import check_csrf, save_or_update_google_user
+from rainfall.login import check_csrf, save_or_update_google_user, register_mastodon_app, redirect_to_instance
+from rainfall.models.mastodon_credential import MastodonCredential
 from rainfall.models.user import User
 
 
@@ -62,6 +64,47 @@ class UserBlueprintFactory:
         return flask.redirect(urljoin(frontend_url, '/sites'))
       else:
         return flask.redirect(urljoin(frontend_url, '/welcome'))
+
+    @user.route('/mastodon/init', methods=['POST'])
+    def mastodon_init():
+      host = flask.request.form.get('host')
+      if host is None:
+        return flask.jsonify(status=400,
+                             error='The field `host` is required'), 400
+
+      if not host.startswith('https://'):
+        host = 'https://' + host
+      parsed = urlparse(host)
+      netloc = parsed.netloc
+
+      stmt = select(MastodonCredential).where(MastodonCredential.host == netloc)
+      result = db.session.execute(stmt)
+      creds = result.fetchone()
+
+      # We have not yet registered an app with this server.
+      if creds is None:
+        creds = register_mastodon_app(netloc)
+      else:
+        creds = creds[0]
+
+      if creds is None:
+        # TODO: Figure out a way to load the MastodonLoginView with an error message.
+        return flask.jsonify(status=400,
+                             error='Could not find that Mastodon host'), 400
+
+      return redirect_to_instance(netloc, creds)
+
+    @self.csrf.exempt
+    @user.route('/mastodon/login')
+    def mastodon_login():
+      code = flask.request.args.get('code')
+      if not code:
+        # TODO: Do something
+        pass
+
+      # TODO: Make a request to the home server and verify the login, then create a User object
+      # and redirect to the welcome or sites page.
+      return flask.redirect(flask.current_app.config['RAINFALL_FRONTEND_URL'])
 
     @user.route('/user/welcome', methods=['POST'])
     @with_current_user
