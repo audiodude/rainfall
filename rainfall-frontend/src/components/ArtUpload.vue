4d83453a8a13f2638be1519a7c234371d526b4a5
Travis Briggs <audiodude@gmail.com>
2024-01-28 09:13:02 -0800

    Refactor uploads so they are specific to the object. Create upload blueprint. Art uploads not implemented yet.

---
+++ rainfall-frontend/src/components/ArtUpload.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import UploadButton from './UploadButton.vue';
+
+export default defineComponent({
+  props: {
+    releaseId: { type: String, required: true },
+  },
+  components: { UploadButton },
+});
+</script>
+
+<template>
+  <div>
+    <div class="bg-gray-500 w-[50%] p-[50%] md:w-[25%] md:p-[25%]"></div>
+    <UploadButton
+      :uploadUrl="`/api/v1/upload/release/${releaseId}`"
+      releaseId=""
+      class="md:mr-[50%]"
+      >Upload art</UploadButton
+    >
+  </div>
+</template>
