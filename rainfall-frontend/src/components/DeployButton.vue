dc9bb37f0f938449615b37a16c62e5ce47ea207c
Travis Briggs <audiodude@gmail.com>
2024-10-20 13:05:13 -0700

    Fix styles in general, and especially for mobile

--- rainfall-frontend/src/components/DeployButton.vue
+++ rainfall-frontend/src/components/DeployButton.vue
@@ -18,52 +18,50 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div class="md:max-w-screen-md md:text-left">
-      <!-- focus:ring-4 focus:outline-none focus:ring-blue-300 inline-flex items-center -->
-      <button
-        id="dropdownDefaultButton"
-        :disabled="!readyForDeploy"
-        data-dropdown-toggle="dropdown"
-        class="inline-flex items-center justify-center cursor-pointer mt-4 w-10/12 md:w-32 p-4 md:py-2 text-xl md:text-base disabled:cursor-auto bg-blue-600 text-grey-200 disabled:bg-blue-400 disabled:text-white hover:bg-blue-800 disabled:hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold text-gray-100 hover:text-white px-4 border border-blue-500 rounded hover:border-transparent disabled:hover:border-blue-500"
-        type="button"
+  <div class="mt-4 md:mt-0 text-center">
+    <!-- focus:ring-4 focus:outline-none focus:ring-blue-300 inline-flex items-center -->
+    <button
+      id="dropdownDefaultButton"
+      :disabled="!readyForDeploy"
+      data-dropdown-toggle="dropdown"
+      class="inline-flex items-center justify-center cursor-pointer w-10/12 md:w-48 p-4 md:py-2 text-xl md:text-base disabled:cursor-auto bg-blue-600 text-grey-200 disabled:bg-blue-400 disabled:text-white hover:bg-blue-800 disabled:hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold text-gray-100 hover:text-white px-4 border border-blue-500 rounded hover:border-transparent disabled:hover:border-blue-500"
+      type="button"
+    >
+      Deploy
+      <svg
+        class="w-2.5 h-2.5 ms-3 relative top-0.5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 10 6"
       >
-        Deploy
-        <svg
-          class="w-2.5 h-2.5 ms-3 relative top-0.5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 10 6"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m1 1 4 4 4-4"
-          />
-        </svg>
-      </button>
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="m1 1 4 4 4-4"
+        />
+      </svg>
+    </button>
 
-      <!-- Dropdown menu -->
-      <div
-        id="dropdown"
-        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+    <!-- Dropdown menu -->
+    <div
+      id="dropdown"
+      class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+    >
+      <ul
+        class="py-2 text-sm text-gray-700 dark:text-gray-200"
+        aria-labelledby="dropdownDefaultButton"
       >
-        <ul
-          class="py-2 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownDefaultButton"
-        >
-          <li>
-            <a
-              :href="downloadUrl"
-              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >Download .ZIP</a
-            >
-          </li>
-        </ul>
-      </div>
+        <li>
+          <a
+            :href="downloadUrl"
+            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >Download .ZIP</a
+          >
+        </li>
+      </ul>
     </div>
   </div>
 </template>
