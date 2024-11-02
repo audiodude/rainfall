454fef59ad054d7a579c34815fc3bacd03d6a6bb
Travis Briggs <audiodude@gmail.com>
2024-11-02 11:07:46 -0700

    Fix lint

--- rainfall-frontend/src/components/DeleteReleaseModal.vue
+++ rainfall-frontend/src/components/DeleteReleaseModal.vue
@@ -48,7 +48,7 @@
             Are you sure you want to delete this Release and all associated songs?
           </h3>
           <button
-            @click="this.$emit('confirm-delete')"
+            @click="$emit('confirm-delete')"
             data-modal-hide="delete-modal"
             type="button"
             class="confirm-delete text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
