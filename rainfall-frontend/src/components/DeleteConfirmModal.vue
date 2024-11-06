1a952f95e28d652e7403bdb3e02559d939622a21
Travis Briggs <audiodude@gmail.com>
2024-11-05 19:22:53 -0800

    Reimplement Flowbite component without magic data-* attributes

--- rainfall-frontend/src/components/DeleteConfirmModal.vue
+++ rainfall-frontend/src/components/DeleteConfirmModal.vue
@@ -1,6 +1,15 @@
-<script>
+<script lang="ts">
 export default {
   props: ['displayMessage'],
+  methods: {
+    onConfirm() {
+      this.$emit('confirm-delete');
+      this.$emit('hide');
+    },
+    onCancel() {
+      this.$emit('hide');
+    },
+  },
 };
 </script>
 
@@ -13,9 +22,9 @@ export default {
     <div class="relative p-4 w-full max-w-md max-h-full">
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <button
+          @click="onCancel()"
           type="button"
           class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-          data-modal-hide="delete-modal"
         >
           <svg
             class="w-3 h-3"
@@ -54,15 +63,14 @@ export default {
             {{ displayMessage }}
           </h3>
           <button
-            @click="$emit('confirm-delete')"
-            data-modal-hide="delete-modal"
+            @click="onConfirm()"
             type="button"
             class="confirm-delete text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
           >
             Yes, delete
           </button>
           <button
-            data-modal-hide="delete-modal"
+            @click="onCancel()"
             type="button"
             class="cancel-delete py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
