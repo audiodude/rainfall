b1f58b7ba306925b1e240698f0164e0ea73e529d
Travis Briggs <audiodude@gmail.com>
2025-01-01 11:52:21 -0500

    Generate site and zip, and upload to minio

--- rainfall/site.py
+++ rainfall/site.py
@@ -1,3 +1,4 @@
+import io
 import logging
 import os
 import re
@@ -8,6 +9,7 @@ from uuid import UUID
 
 import flask
 
+from rainfall import object_storage
 from rainfall.db import db
 from rainfall.models.site import Site
 
@@ -63,6 +65,10 @@ def release_path(data_dir_path, release, override_name=None):
                       secure_filename(name))
 
 
+def file_path(data_dir_path, file):
+  return os.path.join(release_path(data_dir_path, file.release), file.filename)
+
+
 def site_exists(preview_dir_path, site_id):
   dir_ = build_dir(preview_dir_path, site_id)
   return os.path.exists(dir_) and len(os.listdir(dir_)) > 0
@@ -75,6 +81,12 @@ def download_site_objects(data_dir_path, site_id):
       path = file_path(data_dir_path, file)
       # The first path is in the object storage, the second is the local path.
       object_storage.download_file(path=path, output_path=path)
+
+
+def upload_site_objects(preview_dir_path, site_id):
+  path = build_dir(preview_dir_path, site_id)
+  # The first path is the local path, the second is in object storage.
+  object_storage.upload_dir_recursively(path=path, output_path=path)
 
 
 def generate_eno_files(data_dir_path, site_id):
@@ -90,15 +102,22 @@ def generate_eno_files(data_dir_path, site_id):
         description=release.description)
 
     eno_path = os.path.join(release_path(data_dir_path, release), 'release.eno')
-    with open(eno_path, 'w') as f:
-      f.write(release_eno)
+    f = io.BytesIO(release_eno.encode('utf-8'))
+    object_storage.put_object(eno_path, f, 'text/plain')
+
+
+def cleanup_site(data_dir_path, preview_dir_path, site_id):
+  site = db.session.get(Site, UUID(site_id))
+  shutil.rmtree(site_path(data_dir_path, site))
+  shutil.rmtree(build_dir(preview_dir_path, site_id))
+  shutil.rmtree(cache_dir(preview_dir_path, site_id))
 
 
 def generate_site(data_dir_path, preview_dir_path, site_id):
-  download_site_objects(staging_dir_path, data_dir_path, site_id)
-
+  download_site_objects(data_dir_path, site_id)
   generate_eno_files(data_dir_path, site_id)
 
+  # Run faircamp.
   try:
     args = [
         'faircamp', '--catalog-dir',
@@ -111,8 +130,14 @@ def generate_site(data_dir_path, preview_dir_path, site_id):
     log.debug('Faircamp output:\n===STDOUT===\n%s',
               output.stdout.decode('utf-8'))
   except subprocess.CalledProcessError as e:
-    return (False, e.stderr.decode('utf-8'))
-  return (True, None)
+    result = (False, e.stderr.decode('utf-8'))
+  result = (True, None)
+
+  upload_site_objects(preview_dir_path, site_id)
+  upload_zip(preview_dir_path, site_id)
+
+  cleanup_site(data_dir_path, preview_dir_path, site_id)
+  return result
 
 
 def zip_file_path(preview_dir_path, site_id):
@@ -125,6 +150,14 @@ def generate_zip(preview_dir_path, site_id):
   root_dir = zip_file_path(preview_dir_path, site_id)
   out_path = os.path.join(root_dir, 'rainfall_site')
   shutil.make_archive(out_path, 'zip', root_dir=root_dir, base_dir='public')
+
+
+def upload_zip(preview_dir_path, site_id):
+  generate_zip(preview_dir_path, site_id)
+  zip_path = os.path.join(zip_file_path(preview_dir_path, site_id),
+                          'rainfall_site.zip')
+  with open(zip_path, 'rb') as f:
+    object_storage.put_object(zip_path, f, 'application/zip')
 
 
 def delete_file(clz, file_id, user):
