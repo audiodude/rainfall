9846f333433d25dc6268fe665fc53b2518aeb1d8
Travis Briggs <audiodude@gmail.com>
2025-01-06 09:41:53 -0800

    Tweak minio migration script

--- rainfall/migration_util/upload_all_local_files_to_object_storage.py
+++ rainfall/migration_util/upload_all_local_files_to_object_storage.py
@@ -10,33 +10,40 @@ from rainfall.main import create_app
 from rainfall.models import Site
 from rainfall.site import build_dir, cache_dir, secure_filename, site_path
 
+FROM_PREVIEW_DIR = os.environ.get('FROM_PREVIEW_DIR', '/data/preview-data')
+FROM_DATA_DIR = os.environ.get('FROM_DATA_DIR', '/data/song-data')
+
 
 def upload_local_files():
   print('Querying sites...')
   preview_dir_path = flask.current_app.config['PREVIEW_DIR']
   data_dir_path = flask.current_app.config['DATA_DIR']
   for site in Site.query.all():
+    from_site_dir_path = site_path(FROM_DATA_DIR, site)
+    from_build_dir_path = build_dir(FROM_PREVIEW_DIR, str(site.id))
+    from_cache_dir_path = cache_dir(FROM_PREVIEW_DIR, str(site.id))
+
     site_dir_path = site_path(data_dir_path, site)
     build_dir_path = build_dir(preview_dir_path, str(site.id))
     cache_dir_path = cache_dir(preview_dir_path, str(site.id))
 
     try:
       print(f'Uploading site {site_dir_path} to object storage')
-      object_storage.upload_dir_recursively(path=site_dir_path,
+      object_storage.upload_dir_recursively(path=from_site_dir_path,
                                             output_path=site_dir_path)
     except AssertionError:
       print('    X - Directory not found')
 
     try:
       print(f'Uploading {build_dir_path} to object storage')
-      object_storage.upload_dir_recursively(path=build_dir_path,
+      object_storage.upload_dir_recursively(path=from_build_dir_path,
                                             output_path=build_dir_path)
     except AssertionError:
       print('    X - Directory not found')
 
     try:
       print(f'Uploading {cache_dir_path} to object storage')
-      object_storage.upload_dir_recursively(path=cache_dir_path,
+      object_storage.upload_dir_recursively(path=from_cache_dir_path,
                                             output_path=cache_dir_path)
     except AssertionError:
       print('    X - Directory not found')
