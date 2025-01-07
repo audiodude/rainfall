00106ca729c9ef4c67d2a180abc8c273c74c5f00
Travis Briggs <audiodude@gmail.com>
2025-01-06 17:01:40 -0800

    Fix typo with env variable in frontend

--- rainfall-frontend/src/components/PreviewSiteButton.vue
+++ rainfall-frontend/src/components/PreviewSiteButton.vue
@@ -60,7 +60,7 @@ export default {
             this.previewLoading = false;
             this.previewError = 'An error occurred while generating the preview';
           } else {
-            setTimeout(this.pollForPreview, import.meta.env.VITE_PREVIEW_POLL_INTERVAL);
+            setTimeout(this.pollForPreview, import.meta.env.VITE_PREVIEW_POLL_MS);
           }
           return;
         }
