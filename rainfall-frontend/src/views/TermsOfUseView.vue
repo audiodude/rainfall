9395b43707a3a90cc4ebf17461b4f6052def9b45
Travis Briggs <audiodude@gmail.com>
2025-01-09 08:55:19 -0800

    Refactor termly code to its own helper, and tweak it to work in Vue environment

--- rainfall-frontend/src/views/TermsOfUseView.vue
+++ rainfall-frontend/src/views/TermsOfUseView.vue
@@ -1,14 +1,13 @@
-<script type="text/javascript">
-(function (d, s, id) {
-  var js,
-    tjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = 'https://app.termly.io/embed-policy.min.js';
-  tjs.parentNode.insertBefore(js, tjs);
-})(document, 'script', 'termly-jssdk');
+<script lang="ts">
+import { embedTermly } from '@/helpers/termly';
+
+export default {
+  created() {
+    embedTermly();
+  },
+};
 </script>
+
 <template>
   <div class="bg-white">
     <div name="termly-embed" data-id="8d548f55-b957-4759-a0aa-807d9934d6c1"></div>
