1765459f949cd28fe564fea9a3cb66cfa222996e
Travis Briggs <audiodude@gmail.com>
2025-01-09 08:45:01 -0800

    Add privacy policy, cookie policy, and terms of use

--- rainfall-frontend/src/App.vue
+++ rainfall-frontend/src/App.vue
@@ -14,21 +14,38 @@ import NavBar from './components/NavBar.vue';
     <div
       class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between text-gray-300"
     >
-      <span class="block md:inline"
-        >Questions? Bugs?
-        <a class="text-blue-500 hover:underline" href="https://github.com/audiodude/rainfall/issues"
-          >File a Github issue</a
-        ></span
-      >
-      <span class="block md:inline">
-        Feedback also welcome
-        <a
-          class="text-blue-500 hover:underline"
-          href="https://docs.google.com/forms/d/e/1FAIpQLSca4myLs9S0kw1DTA5MOB2OGUDP27f6qaTBELT2oDXjAEyUHw/viewform?usp=header"
-          >here</a
+      <div>
+        <span class="block md:inline"
+          >Questions? Bugs?
+          <a
+            class="text-blue-500 hover:underline"
+            href="https://github.com/audiodude/rainfall/issues"
+            >File a Github issue.</a
+          ></span
         >
-      </span>
+        <span class="block md:inline">
+          We welcome
+          <a
+            class="text-blue-500 hover:underline"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSca4myLs9S0kw1DTA5MOB2OGUDP27f6qaTBELT2oDXjAEyUHw/viewform?usp=header"
+            >feedback</a
+          >
+        </span>
+      </div>
+      <div>
+        <span class="block md:inline mr-4">
+          <a class="text-blue-500 hover:underline" href="/terms">Terms of Use</a>
+        </span>
+        <span class="block md:inline mr-4">
+          <a class="text-blue-500 hover:underline" href="/privacy-policy">Privacy Policy</a>
+        </span>
+
+        <span class="block md:inline">
+          <a class="text-blue-500 hover:underline" href="/cookie-policy">Cookie Policy</a>
+        </span>
+      </div>
     </div>
+    <div></div>
   </footer>
 </template>
 
