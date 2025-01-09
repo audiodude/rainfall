a243b51d0caeba8ca868f19ab96baec56ee7ed29
Travis Briggs <audiodude@gmail.com>
2025-01-09 08:45:01 -0800

    Add privacy policy, cookie policy, and terms of use

---
+++ rainfall-frontend/src/views/CookiePolicyView.vue
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+(function (d, s, id) {
+  var js,
+    tjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = 'https://app.termly.io/embed-policy.min.js';
+  tjs.parentNode.insertBefore(js, tjs);
+})(document, 'script', 'termly-jssdk');
+</script>
+
+<template>
+  <div class="bg-white">
+    <div name="termly-embed" data-id="e074f095-ba3f-476e-a964-ec4e4df892fd"></div>
+  </div>
+</template>
---
+++ rainfall-frontend/src/views/PrivacyPolicyView.vue
@@ -0,0 +1,17 @@
+<script>
+(function (d, s, id) {
+  var js,
+    tjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = 'https://app.termly.io/embed-policy.min.js';
+  tjs.parentNode.insertBefore(js, tjs);
+})(document, 'script', 'termly-jssdk');
+</script>
+
+<template>
+  <div class="bg-white">
+    <div name="termly-embed" data-id="05444eee-5ac2-46c7-a32e-f628bb6b0816"></div>
+  </div>
+</template>
